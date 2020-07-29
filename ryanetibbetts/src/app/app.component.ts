936a103d9af4093bc6c2079f92e7a919d4a31f5a
Ryan <ryan.e.tibbetts@gmail.com>
2020-07-29 16:15:29 -0400

    added image slot and rotating text over it

--- ryanetibbetts/src/app/app.component.ts
+++ ryanetibbetts/src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ryanetibbetts';
+  // title = 'ryanetibbetts';
+  public currentFlavorA = 0;
+  public currentFlavorB = 0;
+  public flavorArrA = ['Welcome', 'Imagine', 'Responsive', 'Greatful', 'Win Win Win No Matter What', 'Another One', 'And Another One'];
+  public flavorArrB = ['Breath', 'All Yours', 'Loyal', 'I Appreciate You', 'Another One', 'Another One', 'And Another One'];
+  public flavorSwap = true;
+
+  ngOnInit() {
+    // useFlavorB tells the algorithm to wait to cycle flavorArrB
+    let useFlavorB = false;
+    setInterval(() => {
+      // everytime the flavor swaps, update the flavor we can't see
+      if (this.flavorSwap) {
+        if(useFlavorB) {
+          if(this.currentFlavorB+1 < this.flavorArrB.length) {
+            this.currentFlavorB++;
+          } else {
+            this.currentFlavorB = 0;
+          }
+        } else {
+          useFlavorB = true;
+        }
+      } else {
+        if(this.currentFlavorA+1 < this.flavorArrA.length) {
+          this.currentFlavorA++;
+        } else {
+          this.currentFlavorA = 0;
+        }
+      }
+      this.flavorSwap = !this.flavorSwap;
+      console.log(this.flavorSwap);
+    }, 5000);
+  }
 }
